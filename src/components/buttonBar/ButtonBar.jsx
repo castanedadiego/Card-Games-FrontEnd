4ddd3cc1a93b4d175809101664fe825ddfcf8352
dcastand <86255880+castanedadiego@users.noreply.github.com>
2021-09-20 12:36:37 -0500

    playingSpace

--- src/components/buttonBar/ButtonBar.jsx
+++ src/components/buttonBar/ButtonBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ButtonBar.css';
 
 export default function ButtonBar() {
+
     return (
         <div className= "bar">
             <div className= "double">Double</div>
---
+++ src/components/playingSpace/PlayingSpace.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ButtonBar from '../buttonBar/ButtonBar'
+import Hand from '../Hand/hand'
+
+export default function PlayingSpace() {
+    return (
+        <div>
+            <Hand/>
+            <ButtonBar/>
+        </div>
+    )
+}
